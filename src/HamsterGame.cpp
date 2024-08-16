593841971d1a8a6ab25d9ec0ce020c2d8c095bfa
sigonasr2 <sigonasr2@gmail.com>
2024-08-16 17:40:36 -0500

    Add border image + setup asset loading and animation loading for images.

--- src/HamsterGame.cpp
+++ src/HamsterGame.cpp
@@ -1,6 +1,10 @@
 #include "HamsterGame.h"
+#include <stdexcept>
 
 geom2d::rect<float>HamsterGame::SCREEN_FRAME{{96,0},{320,288}};
+std::unordered_map<std::string,Animate2D::Animation<int>>HamsterGame::ANIMATIONS;
+std::unordered_map<std::string,Renderable>HamsterGame::GFX;
+const std::string HamsterGame::ASSETS_DIR{"assets/"};
 
 HamsterGame::HamsterGame()
 {
@@ -8,21 +12,47 @@ HamsterGame::HamsterGame()
 }
 
 bool HamsterGame::OnUserCreate(){
+	LoadGraphics();
 	LoadAnimations();
 	return true;
 }
 
+void HamsterGame::LoadGraphics(){
+
+	#undef LoadImage
+	auto LoadImage=[this](std::string_view img){
+		GFX.insert({ASSETS_DIR+std::string(img),Renderable{}});
+		rcode result{GFX[ASSETS_DIR+std::string(img)].Load(ASSETS_DIR+std::string(img))};
+		if(result!=OK)throw std::runtime_error{std::format("Failed to Load Image {}. OLC Rcode: {}",img,int(result))};
+	};
+
+	LoadImage("border.png");
+}
+
+void HamsterGame::LoadAnimations(){
+	
+}
+
 bool HamsterGame::OnUserUpdate(float fElapsedTime){
-	DrawDecal({},
-	gameWindow.FillRectDecal({},{150.f,150.f},WHITE);
+	DrawDecal({},GetGFX("border.png").Decal());
+	gameWindow.FillRectDecal({},{500.f,150.f},WHITE);
 	return true;
+}
+
+const Renderable&HamsterGame::GetGFX(const std::string_view img){
+	if(!GFX.count(ASSETS_DIR+std::string(img)))throw std::runtime_error{std::format("Image {} does not exist!",img)};
+	return GFX[ASSETS_DIR+std::string(img)];
+}
+const Animate2D::Animation<int>&HamsterGame::GetAnimations(const std::string_view img){
+	if(!ANIMATIONS.count(ASSETS_DIR+std::string(img)))throw std::runtime_error{std::format("Animations for {} does not exist!",img)};
+	return ANIMATIONS[ASSETS_DIR+std::string(img)];
 }
 
 bool HamsterGame::OnUserDestroy(){
-	
+	ANIMATIONS.clear();
+	GFX.clear();
 	return true;
 }
-
 
 int main()
 {
--- src/HamsterGame.h
+++ src/HamsterGame.h
@@ -43,15 +43,25 @@ All rights reserved.
 
 class HamsterGame : public olc::PixelGameEngine
 {
+	const static std::string ASSETS_DIR;
 public:
+	enum AnimationStates{
+		DEFAULT
+	};
+
 	HamsterGame();
 	static geom2d::rect<float>SCREEN_FRAME;
-	static std::unordered_map<std::string,Animate2D::Animation<int>>ANIMATIONS;
 	const ViewPort gameWindow{{SCREEN_FRAME.pos,SCREEN_FRAME.pos+vf2d{0.f,SCREEN_FRAME.size.y},SCREEN_FRAME.pos+SCREEN_FRAME.size,SCREEN_FRAME.pos+vf2d{SCREEN_FRAME.size.x,0.f}},{96,0}};
 public:
 	bool OnUserCreate()override final;
 	bool OnUserUpdate(float fElapsedTime)override final;
 	bool OnUserDestroy()override final;
+
+	static const Renderable&GetGFX(const std::string_view img);
+	static const Animate2D::Animation<int>&GetAnimations(const std::string_view img);
 private:
+	void LoadGraphics();
 	void LoadAnimations();
+	static std::unordered_map<std::string,Renderable>GFX;
+	static std::unordered_map<std::string,Animate2D::Animation<int>>ANIMATIONS;
 };
